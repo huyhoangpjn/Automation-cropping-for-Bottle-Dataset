29f24b1444cfabb48ac9cc6ba0b9703ab5d6f39b
huyhoangpjn <hoangodo@gmail.com>
2022-11-13 00:32:24 +0100

    v1

---
+++ app.py
@@ -0,0 +1,73 @@
+from automation import automation
+from PyQt6.QtWidgets import *
+import sys
+
+#Class to create window (cant take function so I create 1 more class to take the automation func as param)
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__(parent=None)
+        self.setWindowTitle("Automation Tool for Sorting Bottle Dataset")
+        self.setFixedSize(400, 100)
+        self.generalLayout = QVBoxLayout()
+        centralWidget = QWidget(self)
+        centralWidget.setLayout(self.generalLayout)
+        self.setCentralWidget(centralWidget)
+        self.createDisplay()
+        self.create_epsillon()
+
+
+    def createDisplay(self):
+        self.display = QVBoxLayout()
+        self.button_folder = QPushButton("Your folder")
+        self.display.addWidget(self.button_folder)
+        self.generalLayout.addLayout(self.display)
+        self.button_run = QPushButton("Run")
+        self.display.addWidget(self.button_run)
+        self.generalLayout.addLayout(self.display)
+
+    def create_epsillon(self):
+        self.type = QFormLayout()
+        self.epsillon = QLineEdit(self)
+        self.type.addRow("Epsillon >= 0 (default = 0)",self.epsillon)
+        self.generalLayout.addLayout(self.type)
+    
+    ### :))) Basically this func belongs to second class, but this class uses attribute from Qtpy -> it can implement the choosing folder func easier
+    def onclick_folder(self):
+        self.path =  str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+
+
+#This class implement automation
+class App_run:
+    def __init__(self, view, automation):
+        self._view = view
+        self.automation = automation
+        self.run()
+
+    def onclick_run(self):
+        try:
+            if self._view.epsillon.text() == "":
+                self.automation(self._view.path, 0)
+            elif int(self._view.epsillon.text()) < 0:
+                self.automation(self._view.path, 0)
+            else:
+                self.automation(self._view.path, int(self._view.epsillon.text()))
+        except:
+            pass
+
+    def run(self):
+        try:
+            self._view.button_folder.clicked.connect(self._view.onclick_folder)
+        except:
+            pass
+
+        try:
+            self._view.button_run.clicked.connect(self.onclick_run)
+        except:
+            pass
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = Window()
+    app_run = App_run(window,automation)
+    window.show()
+    sys.exit(app.exec())
---
+++ automation.py
@@ -0,0 +1,56 @@
+import os 
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+def automation(path, epsillon):
+    dir_list = os.listdir(path)
+    os.chdir(path)
+    out_path = os.path.join(path, ".out")
+    try:    
+        os.mkdir(out_path)
+    except:
+        pass
+
+    for file in dir_list:
+        os.chdir(path)
+        if '.txt' in file:
+            with open(file, 'r') as f:
+                content = f.read().split(" ")
+                index = content[0]
+                f.close()
+            file = file.split('.')
+            img = cv2.imread(file[0]+'.jpg')
+            X = int(float(content[2])*img.shape[0])
+            Y = int(float(content[1])*img.shape[1])
+            W = int(float(content[3])*img.shape[0])
+            H = int(float(content[4])*img.shape[1])
+            #Convert to square size
+            if W>H:
+                H = W
+            else:
+                W = H
+            #Avoid overflow the edge, in this case, a problem is on data -> Can't fix the size to square size
+            #Now, we call (a,b),(c,d) is 2 coordinates of 2 vertices
+            
+            a = X-int(W/2) - int(epsillon)
+            b = Y-int(H/2) - int(epsillon)
+            c = X+int(W/2) + int(epsillon)
+            d = Y+int(H/2) + int(epsillon)
+
+            if X-int(W/2) < 0:
+                a = 0
+            if Y-int(H/2) < 0:
+                b = 0
+            if X+int(W/2) > img.shape[0]:
+                c = img.shape[0]
+            if Y+int(H/2) > img.shape[1]:
+                d = img.shape[1]
+            img = img[a:c,b:d]
+
+            #Save img
+            name_img = file[0].replace(",","") + '.jpg'
+            os.chdir(out_path)
+            cv2.imwrite(name_img, img)
+
+        
